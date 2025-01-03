eeee34219d06502202b3acb46bdca4e40347a951
Park Seunghan <45682868+KOREAparksh@users.noreply.github.com>
2025-01-03 13:25:26 +0900

    initial-flutter  (#3)
    * flutter config 분리
    
    * Readme 추가

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,20 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String flavor = "none";
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 300), () {
+      flavor = const String.fromEnvironment('FLAVOR', defaultValue: 'err');
+      setState(() {});
     });
   }
 
@@ -49,8 +59,9 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              'Flavor: $flavor',
+              style: Theme.of(context).textTheme.headlineMedium,
             ),
             Text(
               '$_counter',
